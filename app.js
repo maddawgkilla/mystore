682c3f6f50cb602ab7e82c8d0d322d7db1b4a3ac
Sami FP Laptop <maddawgkilla@gmail.com>
2019-01-21 18:08:54 +0500

    Second Commit

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 const express = require("express");
-require('dotenv').config();
-var winston = require("winston");
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-      //
-      // - Write to all logs with level `info` and below to `combined.log` 
-      // - Write all logs error (and below) to `error.log`.
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' })
-    ]
-  });
-   
-  //
-  // If we're not in production then log to the `console` with the format:
-  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  // 
-  if (process.env.NODE_ENV !== 'production') {
-      console.log("this is production");
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple()
-    }));
-  }
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
 var app = express();
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get('/', (req,res) => {
     res.send("We in the building");
